f2cb842b5d440393563b481c497df39935ee7c9c
yijiu824 <92504141+yijiu824@users.noreply.github.com>
2021-12-29 20:03:21 +0800

    Update DeleteNote.java

--- DeleteNote.java
+++ DeleteNote.java
@@ -9,7 +9,7 @@ import java.sql.PreparedStatement;
 import com.google.gson.reflect.TypeToken;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/DeleteNotepad ")
+@WebServlet(urlPatterns = "/DeleteNote")
 public class DeleteNote extends HttpServlet {
    final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
    final static String URL = "jdbc:mysql://180.76.189.89/linux_final";
