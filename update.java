df487312521aa6ec43533b4b046c94e0e726349f
yijiu824 <92504141+yijiu824@users.noreply.github.com>
2021-12-29 20:04:12 +0800

    Update update.java

--- update.java
+++ update.java
@@ -9,7 +9,7 @@ import java.sql.PreparedStatement;
 import com.google.gson.reflect.TypeToken;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/CreateNote")
+@WebServlet(urlPatterns = "/update")
 public class update extends HttpServlet {
    final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
    final static String URL = "jdbc:mysql://180.76.189.89/linux_final";
